44bbc6711e2133d0efe314b3f0592aaabbfa5828
AskhatZRPV <askhatzaripov@gmail.com>
2023-12-10 19:54:22 +0300

    feat: config update

--- internal/core/config/config.go
+++ internal/core/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 type Config struct {
-	Env      string `yaml:"env" env-default:"local"`
-	Database `yaml:"database"`
+	Env        string `yaml:"env" env-default:"local"`
+	Database   `yaml:"database"`
+	HttpServer `yaml:"http_server"`
 }
 
 type Database struct {
@@ -11,4 +12,9 @@ type Database struct {
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 	Name     string `yaml:"name"`
+}
+
+type HttpServer struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
 }
